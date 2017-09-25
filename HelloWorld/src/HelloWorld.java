afa19dc7a19231397379e1129afb2d8f51fb5a8e
ade <ade@10.10.7.252>
2017-09-25 16:31:06 +0700

    initial

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Banzaiii\n");
+		greetDisplay();
+	}
+	private static void greetDisplay() {
+		System.out.println("hello world");
+	}
+}
